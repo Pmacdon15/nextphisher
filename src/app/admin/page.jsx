e199158c34578140cb10e2535253d1d8d5058dbf
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-05 14:49:57 -0600

    new layout and loading for admin page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import Image from "next/image";
-import adminImage from "../../../public/admin.png";
+// import Image from "next/image";
+// import adminImage from "../../../public/admin.png";
 import { useState, useEffect } from "react";
 import adminStyles from "./adminStyles.module.css";
 import Button from "@mui/material/Button";
@@ -50,57 +50,44 @@ export default function AdminDashboard() { // Change component name
   }
 
   return (
-    <div className={adminStyles.dashboardContainer}>
-      <div className={adminStyles.contentContainer}>
-        <Image src={adminImage} alt="Next Pisher" className={adminStyles.image} />
-        <h2 className={adminStyles.title}>Admin Dashboard</h2>
-        {webSitesInProject !== null && webSitesInProject !== undefined ? (
-          webSitesInProject.length > 0 ? (
-            <div>
-              <div className={adminStyles.contentRow}>
-                {webSitesInProject.map((webSite, index) => (
-                  <Link key={webSite.name} href={`/admin/QrCode/${webSite.name}`}>
-                    <div className={adminStyles.siteData}>
-                      <p className={adminStyles.par}>
-                        Project Name: {webSite.name}
-                      </p>
-                    </div>
-                  </Link>
-                ))}
-              </div>
-
-              <div className={adminStyles.contentRow}>
-                <Button variant="contained" color="success" style={{ margin: "1%" }}>
-                  <Link href="/admin/decoy">Decoy Controller</Link>
-                </Button>
-                <Button variant="contained" color="success" style={{ margin: "1%" }}>
-                  <Link href="/admin/userData">User Data</Link>
-                </Button>
-                <Button
-                  onClick={logout}
-                  variant="contained"
-                  color="success"
-                  style={{ margin: "1%" }}
-                >
-                  Logout
-                </Button>
-              </div>
-            </div>
-          ) : (
-            <p>No projects available</p>
-          )
-        ) : (
+    <div>
+      <h2 className={adminStyles.title}>Admin Dashboard</h2>
+      {webSitesInProject.length > 0 ? (
+        <div>
           <div className={adminStyles.contentRow}>
-            <p>Loading or Not Signed In</p>
-            <Button variant="contained" color="success">
-              <Link href="/">Login</Link>
+            {webSitesInProject.map((webSite, index) => (
+              <Link key={webSite.name} href={`/admin/QrCode/${webSite.name}`}>
+                <div className={adminStyles.siteData}>
+                  <p className={adminStyles.par}>
+                    Project Name: {webSite.name}
+                  </p>
+                </div>
+              </Link>
+            ))}
+          </div>
+
+          <div className={adminStyles.contentRow}>
+            <Button variant="contained" onClick={() => router.push("/admin/decoy")} color="success" style={{ margin: "1%" }}>
+              Decoy Controller
+            </Button>
+            <Button variant="contained" onClick={() => router.push("/admin/userData")} color="success" style={{ margin: "1%" }}>
+              User Data
+            </Button>
+            <Button
+              onClick={logout}
+              variant="contained"
+              color="success"
+              style={{ margin: "1%" }}
+            >
+              Logout
             </Button>
           </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <p>No projects available</p>
+      )}
     </div>
   );
-
-};
+}
 
 
