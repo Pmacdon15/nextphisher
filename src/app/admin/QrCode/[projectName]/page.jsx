ce13f6bc7fae8f8b7b64ff0ffae7fea4d46068a2
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-10 12:44:00 -0600

    back button work with new approach

--- src/app/admin/QrCode/[projectName]/page.jsx
+++ src/app/admin/QrCode/[projectName]/page.jsx
@@ -1,97 +1,109 @@
-"use client";
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import adminStyles from "../../adminStyles.module.css";
-import adminImage from "../../../../../public/admin.png";
-import Button from "@mui/material/Button";
-import Link from "next/link";
+'use server';
+import ClientComponent from "@/components/QrCode/clientComponent.jsx";
 
-export default function QrCode({ params }) {
-  const webSiteZToShowQrCodeFor = params.projectName;
-  const [qrCodeUrl, setQrCodeUrl] = useState("");
-  const [userAuth, setUserAuth] = useState(false);
-
-  useEffect(() => {
-    try {
-      const fetchData = async () => {
-        const response = await fetch("/api/admin/auth");
-        const data = await response.json();
-        if (data.message === "Authorized") {
-          setUserAuth(true);
-        }
-      };
-      fetchData();
-    } catch (error) {
-      console.error("Error reading file", error);
-    }
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      let url = "";
-      try {
-        const serverIp = await fetch("/api/admin/getIp");
-        const ip = await serverIp.json();
-        console.log(ip);
-        if (ip.message === "Unauthorized") {
-          throw new Error("Unauthorized");
-        }
-
-        url = "http://" + ip.ip + ":3000/" + webSiteZToShowQrCodeFor;
-
-      } catch (error) {
-        console.error("Error getting server IP address error: ", error);
-      }
-
-      try {
-        const response = await fetch(
-          `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`,
-          {
-            cors: "no-cors",
-          }
-        );
-        if (!response.ok) {
-          throw new Error("Failed to fetch QR code");
-        }
-        const data = await response.blob();
-        const imageUrl = URL.createObjectURL(data);
-        setQrCodeUrl(imageUrl);
-      } catch (error) {
-        console.error("Error getting Qr Code");
-      }
-    };
-
-    fetchData();
-  }, [webSiteZToShowQrCodeFor]); // Include webSiteZToShowQrCodeFor in the dependency array
-
-  console.log(qrCodeUrl);
-
+export default async function QrCodePage() {
   return (
-    <div>
-        <h2 className={adminStyles.title}>Qr Code Link</h2>
-        {qrCodeUrl ? (
-          <div className={adminStyles.contentRow}>
-            <Image
-              width={250}
-              height={250}
-              src={qrCodeUrl}
-              alt="QR Code"
-              className={adminStyles.image}
-            />{" "}
-          </div>
-        ) : (
-          userAuth? (
-            <p>QR Code loading....</p>
-          ) : (
-            <p>Unauthorized</p>
-          )
-        )}
-        <div className={adminStyles.contentRow}>
-          <Button variant="contained" color="success">
-            <Link href="/admin/">Back</Link>
-          </Button>
-        </div>
-      </div>
+    
+      <ClientComponent />
     
   );
 }
+
+
+// "use client";
+// import { useState, useEffect } from "react";
+// import Image from "next/image";
+// import adminStyles from "../../adminStyles.module.css";
+// import adminImage from "../../../../../public/admin.png";
+// import Button from "@mui/material/Button";
+// import Link from "next/link";
+
+// export default function QrCode({ params }) {
+//   const webSiteZToShowQrCodeFor = params.projectName;
+//   const [qrCodeUrl, setQrCodeUrl] = useState("");
+//   const [userAuth, setUserAuth] = useState(false);
+
+//   useEffect(() => {
+//     try {
+//       const fetchData = async () => {
+//         const response = await fetch("/api/admin/auth");
+//         const data = await response.json();
+//         if (data.message === "Authorized") {
+//           setUserAuth(true);
+//         }
+//       };
+//       fetchData();
+//     } catch (error) {
+//       console.error("Error reading file", error);
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       let url = "";
+//       try {
+//         const serverIp = await fetch("/api/admin/getIp");
+//         const ip = await serverIp.json();
+//         console.log(ip);
+//         if (ip.message === "Unauthorized") {
+//           throw new Error("Unauthorized");
+//         }
+
+//         url = "http://" + ip.ip + ":3000/" + webSiteZToShowQrCodeFor;
+
+//       } catch (error) {
+//         console.error("Error getting server IP address error: ", error);
+//       }
+
+//       try {
+//         const response = await fetch(
+//           `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`,
+//           {
+//             cors: "no-cors",
+//           }
+//         );
+//         if (!response.ok) {
+//           throw new Error("Failed to fetch QR code");
+//         }
+//         const data = await response.blob();
+//         const imageUrl = URL.createObjectURL(data);
+//         setQrCodeUrl(imageUrl);
+//       } catch (error) {
+//         console.error("Error getting Qr Code");
+//       }
+//     };
+
+//     fetchData();
+//   }, [webSiteZToShowQrCodeFor]); // Include webSiteZToShowQrCodeFor in the dependency array
+
+//   console.log(qrCodeUrl);
+
+//   return (
+//     <div>
+//         <h2 className={adminStyles.title}>Qr Code Link</h2>
+//         {qrCodeUrl ? (
+//           <div className={adminStyles.contentRow}>
+//             <Image
+//               width={250}
+//               height={250}
+//               src={qrCodeUrl}
+//               alt="QR Code"
+//               className={adminStyles.image}
+//             />{" "}
+//           </div>
+//         ) : (
+//           userAuth? (
+//             <p>QR Code loading....</p>
+//           ) : (
+//             <p>Unauthorized</p>
+//           )
+//         )}
+//         <div className={adminStyles.contentRow}>
+//           <Button variant="contained" color="success">
+//             <Link href="/admin/">Back</Link>
+//           </Button>
+//         </div>
+//       </div>
+    
+//   );
+// }
---
+++ src/components/QrCode/clientComponent.jsx
@@ -0,0 +1,20 @@
+'use client';
+import QrCodeStyles from "@/components/QrCode/QrCode.module.css";
+import Button from "@mui/material/Button";
+import { useRouter} from "next/navigation";
+
+export default function ClientComponent({children}) {
+    const router = useRouter();
+    return (
+    <div>
+        <h2 className={QrCodeStyles.title}>Qr Code Link</h2>
+        {children}
+        <div className={QrCodeStyles.dataColumn}>
+          <Button  onClick={() => router.push(`/admin`)} variant="contained" color="success">
+            Back
+          </Button>
+        </div>
+      </div>
+    
+  );
+}
